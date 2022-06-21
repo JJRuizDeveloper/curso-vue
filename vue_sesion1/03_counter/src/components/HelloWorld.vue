f571cbb7105b36b5e828b2a28a0928983336cacc
JuanjoRuiz <juanjose.ruiz.munoz@gmail.com>
2022-06-21 22:38:16 +0200

    Sesion_1 complete

---
+++ vue_sesion1/03_counter/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ time }}</h1>
+    <button @click="decrease">Decrementar</button>
+    <button @click="increase">Incrementar</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  setup() {
+    let time = ref(0)
+
+    function decrease ():void {
+      time.value--
+    }
+
+    function increase ():void {
+      time.value++
+    }
+
+    return {
+      time, decrease, increase
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue_sesion1/04_images/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <img :src="require(`@/assets/${imageUri}`)" />
+    <button @click="spain">Español</button><button @click="france">Francés</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  data() {
+    return {
+      imageUri: "spain.png"
+    }
+  },
+  methods: {
+    spain: function () {
+      this.imageUri = "spain.png"
+    },
+    france: function () {
+      this.imageUri = "france.png"
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
