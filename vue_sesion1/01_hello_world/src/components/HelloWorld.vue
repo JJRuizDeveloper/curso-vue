34e7066805cc13aeac81c7d72ecb35f19c3d1b40
JuanjoRuiz <juanjose.ruiz.munoz@gmail.com>
2022-06-21 22:35:24 +0200

    exercise 01

---
+++ vue_sesion1/01_hello_world/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ hello }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, Ref, ref } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  /* OPTIONS API */
+  /* data() {
+    return {
+      hello : "Hola mundo desde data"
+    }
+  }*/
+  /* COMPOSITION API */
+  setup() {
+    let hello:Ref<string> = ref("Hola mundo desde composition")
+    return {
+      hello
+    }
+  }
+});
+</script>
